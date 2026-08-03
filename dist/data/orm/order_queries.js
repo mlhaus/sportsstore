ac656a43cf8342bcb7784cd1cc599631b5f317bd
Marc Hauschildt <marc.hauschildt@gmail.com>
2026-08-03 12:52:24 -0500

    initial commit

---
+++ dist/data/orm/order_queries.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AddOrderQueries = AddOrderQueries;
+const order_models_1 = require("./models/order_models");
+const customer_models_1 = require("./models/customer_models");
+const queryConfig = {
+    include: [
+        { model: order_models_1.AddressModel, as: "address" },
+        { model: customer_models_1.CustomerModel, as: "customer" }
+    ],
+    raw: true, nest: true
+};
+function AddOrderQueries(Base) {
+    return class extends Base {
+        getOrder(id) {
+            return order_models_1.OrderModel.findByPk(id, queryConfig);
+        }
+        getOrders(excludeShipped) {
+            return order_models_1.OrderModel.findAll(excludeShipped ?
+                { ...queryConfig, where: { shipped: false } } : queryConfig);
+        }
+    };
+}
